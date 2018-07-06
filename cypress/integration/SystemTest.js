82391f65f3fc70988cdcd9736e9bc644ee8308fd
stein_korsveien@msn.com <stein_korsveien>
2018-07-06 11:40:51 +0200

    Add product component issu #1

--- cypress/integration/SystemTest.js
+++ cypress/integration/SystemTest.js
@@ -5,8 +5,8 @@ describe("Spesifies client behaivior", function()  {
 	
 	it('should diapley hello world in div', function() {
 		cy.visit('http://localhost:3000')
-	   const element =cy.get('p');
-	   element.should('contain','Hello world!!!');
+	   const element =cy.get('div');
+		expect(element).to.exist
 	   });
 	
 	
