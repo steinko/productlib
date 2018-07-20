4dc92aff478bf2cf80c5801499f225f7e2da803a
stein_korsveien@msn.com <stein_korsveien>
2018-07-20 02:20:23 +0200

    Developed issue #12

--- cypress/integration/ProductList-spec.js
+++ cypress/integration/ProductList-spec.js
@@ -9,9 +9,11 @@ configure({ adapter: new Adapter() })
 
 describe('ProductList unit testing', () => {
   var productList
+  
   beforeEach(() => {
     productList = shallow(<ProductList />)
   })
+  
   it('Should display two products in a productlist', () => {
     expect(productList.exists()).to.equal(true)
     expect(productList.find('div').hasClass('ui unstackable items')).to.equal(true)
@@ -20,7 +22,7 @@ describe('ProductList unit testing', () => {
     expect(product1.prop('title')).to.equal('Yellow Pale')
     expect(product1.prop('description')).to.equal('On demand sand castle expertise')
     expect(product1.prop('url')).to.equal('#1')
-    expect(product1.prop('votes')).to.equal('41')
+    expect(product1.prop('votes')).to.equal(41)
     expect(product1.prop('submitterAvatarUrl')).to.equal('../../src/images/avatar.png')
     expect(product1.prop('productImageUrl')).to.equal('../../src/images/Banana-Snowboard.png')
     const product2 = productList.childAt(1)
@@ -28,8 +30,34 @@ describe('ProductList unit testing', () => {
     expect(product2.prop('title')).to.equal('Strawberries')
     expect(product2.prop('description')).to.equal('Strawberries Forever')
     expect(product2.prop('url')).to.equal('#2')
-    expect(product2.prop('votes')).to.equal('46')
+    expect(product2.prop('votes')).to.equal(46)
     expect(product2.prop('submitterAvatarUrl')).to.equal('../../src/images/FemaleAvatar.png')
     expect(product2.prop('productImageUrl')).to.equal('../../src/images/sugarinStrawberries.png')
-  })
+   })
+  
+   it('Should call handleProductUpVoute function when on click in Product' , () => {
+    const productiListInstance = productList.instance()
+    cy.spy(productiListInstance,"handleProductUpVote")
+    const product = shallow(<Product onVote = {productiListInstance.handleProductUpVote} />)
+    product.find('div.header').find('a').simulate('click')
+    expect(productiListInstance.handleProductUpVote).to.be.called
+   })
+   
+   it('Should increment the vote for the product' , ()=> {
+    const productiListInstance = productList.instance()
+    const product = shallow(<Product id = {3} onVote = {productiListInstance.handleProductUpVote} votes = {1} />)
+    productiListInstance.handleProductUpVote(3)
+    const  productInstance = product.instance()  
+    expect(productInstance.props.votes).to.equal(2)   
+   })
+   
+   it('Should create a undefined state for products' , ()=> {
+    expect(productList.state.products).to.equal(undefined)
+   })
+   
+   it('Should increment products votes ' , ()=> {
+    const product = productList.childAt(0)
+    product.find('div.header').find('a').simulate('click')
+    expect( product.find('div.header').text()).to.equal('42')
+   })
 })
